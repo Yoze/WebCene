683612641d6a30aa4a45ca23269ccd6b171d7375
ViceYO <viceyo@outlook.com>
2017-10-17 22:21:25 +0200

    testiranje agility html paketa

--- WebCene.UI/MainWindow.Designer.cs
+++ WebCene.UI/MainWindow.Designer.cs
@@ -28,20 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnDodajLink = new System.Windows.Forms.Button();
+            this.btnUcitajHTML = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnDodajLink
+            // 
+            this.btnDodajLink.Location = new System.Drawing.Point(39, 28);
+            this.btnDodajLink.Name = "btnDodajLink";
+            this.btnDodajLink.Size = new System.Drawing.Size(165, 44);
+            this.btnDodajLink.TabIndex = 0;
+            this.btnDodajLink.Text = "Dodaj link";
+            this.btnDodajLink.UseVisualStyleBackColor = true;
+            // 
+            // btnUcitajHTML
+            // 
+            this.btnUcitajHTML.Location = new System.Drawing.Point(52, 118);
+            this.btnUcitajHTML.Name = "btnUcitajHTML";
+            this.btnUcitajHTML.Size = new System.Drawing.Size(133, 38);
+            this.btnUcitajHTML.TabIndex = 1;
+            this.btnUcitajHTML.Text = "Učitaj HTML";
+            this.btnUcitajHTML.UseVisualStyleBackColor = true;
+            this.btnUcitajHTML.Click += new System.EventHandler(this.btnUcitajHTML_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(56, 291);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(50, 20);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "label1";
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(882, 584);
+            this.ClientSize = new System.Drawing.Size(1105, 605);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnUcitajHTML);
+            this.Controls.Add(this.btnDodajLink);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Web Cene";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnDodajLink;
+        private System.Windows.Forms.Button btnUcitajHTML;
+        private System.Windows.Forms.Label lblResult;
     }
 }
 
--- WebCene.UI/MainWindow.cs
+++ WebCene.UI/MainWindow.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HtmlAgilityPack;
 
 namespace WebCene.UI
 {
@@ -15,6 +16,25 @@ namespace WebCene.UI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnUcitajHTML_Click(object sender, EventArgs e)
+        {
+            // eponuda
+
+            var html =
+                @"http://www.eponuda.com/laptopovi/asus-x541na-go020-15-6-intel-n3350-dual-core-1-1ghz-2-4ghz-4gb-1tb-odd-crno-zlatni-laptop-cena-411347";
+
+            
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(html);
+
+
+            var cenaDatumAzuriranja = htmlDoc.DocumentNode.SelectSingleNode("//table[1]/tr[4]/td[3]/div");
+            var cena = htmlDoc.DocumentNode.SelectSingleNode("//table[1]/tr[4]/td[4]/b");
+
+            lblResult.Text = cena.InnerHtml + " datum: " + cenaDatumAzuriranja.InnerHtml;
+
         }
     }
 }
